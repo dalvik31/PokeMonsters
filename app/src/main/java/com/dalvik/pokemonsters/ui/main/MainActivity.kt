8b04d42b037376be690cc8d4536fd3ada0724f44
emmanuelpacheco <eeph31@gmail.com>
2024-08-16 23:05:47 -0600

    update screen oroentation

--- app/src/main/java/com/dalvik/pokemonsters/ui/main/MainActivity.kt
+++ app/src/main/java/com/dalvik/pokemonsters/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.dalvik.pokemonsters.ui.main
 
 import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
@@ -15,9 +16,11 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
         setContentView(binding.root)
         setUpBottomNavigationView()
     }
